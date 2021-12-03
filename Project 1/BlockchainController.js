06fc4d64e2b7ffba83b8129bccf01fcfb072e4b7
Aram Bayadyan <aram.bv@gmail.com>
2021-12-03 08:14:28 +0400

    small update to Project 1

--- Project 1/BlockchainController.js
+++ Project 1/BlockchainController.js
@@ -118,8 +118,7 @@ class BlockchainController {
   validateChain() {
     this.app.get('/chain/validate', async (req, res) => {
       try {
-        const validationResult = await this.blockchain.validateChain();
-        return res.status(200).json(validationResult);
+        return res.status(200).json(await this.blockchain.validateChain());
       } catch (error) {
         return res.status(500).send(error.message);
       }
